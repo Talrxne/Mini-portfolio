7ac3d221739074db880acd75149367306efb3a11
Talrxne <110570530+Talrxne@users.noreply.github.com>
2022-10-22 00:43:52 -0300

    Commit Portfolio

---
+++ src/JS/Index.js
@@ -0,0 +1,33 @@
+const abas = document.querySelectorAll(".aba");
+
+
+abas.forEach(aba => {
+    aba.addEventListener("click", function () {
+        if (aba.classList.contains("selecionado")) {
+            return;
+        }
+
+
+        const abaSelecionada = document.querySelector(".aba.selecionado")
+
+        abaSelecionada.classList.remove("selecionado")
+
+        aba.classList.add("selecionado")
+
+        const informacaoSelecionada = document.
+            querySelector(".informacao.selecionado");
+        informacaoSelecionada.classList.remove ("selecionado");
+        const idDoElementoDeInformacoes =
+            `informacao-${aba.id}`
+
+        const informacaoASerMostrada = document.
+            getElementById(idDoElementoDeInformacoes)
+
+        informacaoASerMostrada.classList.add("selecionado")
+
+    });
+
+
+
+
+});
